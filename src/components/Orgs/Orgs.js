d2b7c2c66c5acd3b1bd6eb32267a927bef921441
Макар Зарчуков <makzarchukov@gmail.com>
2021-06-02 04:10:43 +0500

    added orgs, devs and reports content

--- src/components/Orgs/Orgs.js
+++ src/components/Orgs/Orgs.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styles from "./Orgs.module.css"
+
 import SideNav from "../SideNav/SideNav";
 import HeaderNav from "../HeaderNav/HeaderNav";
 
@@ -11,7 +12,60 @@ export default function Orgs() {
             <SideNav/>
             <HeaderNav pageName="Список организаций"/>
             <div className={styles.content}>
-                Список организаций
+                <form className="search">
+                    <input name="search" placeholder="Поиск" type="search"/>
+                </form>
+                <button className={styles.addButton} type="submit" name="report">
+                    + Добавить организацию
+                </button>
+                <table className={styles.table}>
+                    <tr className={styles.head}>
+                        <th className={styles.emptyColumn}/>
+                        <th className={styles.fColumn}>Организация</th>
+                        <th className={styles.sColumn}>Статус</th>
+                        <th className={styles.tColumn}>Владелец</th>
+                        <th className={styles.foColumn}>Представители</th>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>ООО "Крокодилы"</p>
+                        </td>
+                        <td className={styles.sColumn}>
+                            <div className={`${styles.status} ${styles.acting}`}>Действующая</div>
+                        </td>
+                        <td className={styles.tColumn}>
+                            <p>Иванов Иван</p>
+                        </td>
+                        <td className={styles.foColumn}><p>29 сотрудников</p></td>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>ИП Кочергин Константин</p>
+                        </td>
+                        <td className={styles.sColumn}>
+                            <div className={`${styles.status} ${styles.acting}`}>Действующая</div>
+                        </td>
+                        <td className={styles.tColumn}>
+                            <p>Кочергин Константин</p>
+                        </td>
+                        <td className={styles.foColumn}><p>8 сотрудников</p></td>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>ООО "Крокодилы"</p>
+                        </td>
+                        <td className={styles.sColumn}>
+                            <div className={`${styles.status} ${styles.noacting}`}>Не действующая</div>
+                        </td>
+                        <td className={styles.tColumn}>
+                            <p>Иванов Иван</p>
+                        </td>
+                        <td className={styles.foColumn}><p>5 сотрудников</p></td>
+                    </tr>
+                </table>
             </div>
         </div>
     )
--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import styles from "../Reports/Reports.module.css"
 
+import star from "../../images/star.svg"
+import stargold from "../../images/stargold.svg"
+
 import SideNav from "../SideNav/SideNav";
 import HeaderNav from "../HeaderNav/HeaderNav";
 
@@ -12,7 +15,153 @@ export default function Reports() {
             <SideNav/>
             <HeaderNav pageName="Обращения"/>
             <div className={styles.content}>
-                Обращения
+                <form className={styles.search}>
+                    <input name="search" placeholder="Поиск" type="search"/>
+                </form>
+                <table className={styles.table}>
+                    <tr className={styles.head}>
+                        <th className={styles.emptyColumn}/>
+                        <th className={styles.fColumn}>Автор</th>
+                        <th className={styles.f1Column}>Организация</th>
+                        <th className={styles.f2Column}>Важность</th>
+                        <th className={styles.sColumn}>Тема</th>
+                        <th className={styles.tColumn}>Ответственный</th>
+                        <th className={styles.foColumn}>Статус</th>
+                        <th className={styles.fiColumn}>ПО</th>
+                        <th className={styles.siColumn}>Модуль</th>
+                        <th className={styles.seColumn}>Дата создания</th>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>Иванов Иван</p>
+                        </td>
+                        <td className={styles.f1Column}>
+                            <p>ООО "Крокодилы"</p>
+                        </td>
+                        <td className={styles.f2Column}>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                        </td>
+                        <td className={styles.sColumn}><p>Добавление нового сотрудника</p></td>
+                        <td className={styles.tColumn}>
+                            <p>Пётр Петров</p>
+                        </td>
+                        <td className={styles.foColumn}>
+                            <div className={styles.status}>Зарегистрировано</div>
+                        </td>
+                        <td className={styles.fiColumn}><p>DATAPK</p></td>
+                        <td className={styles.siColumn}><p>Техническое обслуживание</p></td>
+                        <td className={styles.seColumn}><p>22 апреля 2021</p></td>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>Иванов Иван</p>
+                        </td>
+                        <td className={styles.f1Column}>
+                            <p>ООО "Крокодилы"</p>
+                        </td>
+                        <td className={styles.f2Column}>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                        </td>
+                        <td className={styles.sColumn}><p>Добавление нового сотрудника</p></td>
+                        <td className={styles.tColumn}>
+                            <p>Пётр Петров</p>
+                        </td>
+                        <td className={styles.foColumn}>
+                            <div className={styles.status}>Зарегистрировано</div>
+                        </td>
+                        <td className={styles.fiColumn}><p>DATAPK</p></td>
+                        <td className={styles.siColumn}><p>Техническое обслуживание</p></td>
+                        <td className={styles.seColumn}><p>22 апреля 2021</p></td>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>Иванов Иван</p>
+                        </td>
+                        <td className={styles.f1Column}>
+                            <p>ООО "Крокодилы"</p>
+                        </td>
+                        <td className={styles.f2Column}>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                        </td>
+                        <td className={styles.sColumn}><p>Добавление нового сотрудника</p></td>
+                        <td className={styles.tColumn}>
+                            <p>Пётр Петров</p>
+                        </td>
+                        <td className={styles.foColumn}>
+                            <div className={styles.status}>Зарегистрировано</div>
+                        </td>
+                        <td className={styles.fiColumn}><p>DATAPK</p></td>
+                        <td className={styles.siColumn}><p>Техническое обслуживание</p></td>
+                        <td className={styles.seColumn}><p>22 апреля 2021</p></td>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>Иванов Иван</p>
+                        </td>
+                        <td className={styles.f1Column}>
+                            <p>ООО "Крокодилы"</p>
+                        </td>
+                        <td className={styles.f2Column}>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                        </td>
+                        <td className={styles.sColumn}><p>Добавление нового сотрудника</p></td>
+                        <td className={styles.tColumn}>
+                            <p>Пётр Петров</p>
+                        </td>
+                        <td className={styles.foColumn}>
+                            <div className={styles.status}>Зарегистрировано</div>
+                        </td>
+                        <td className={styles.fiColumn}><p>DATAPK</p></td>
+                        <td className={styles.siColumn}><p>Техническое обслуживание</p></td>
+                        <td className={styles.seColumn}><p>22 апреля 2021</p></td>
+                    </tr>
+                    <tr className={styles.report1}>
+                        <td className={styles.emptyColumn}/>
+                        <td className={styles.fColumn}>
+                            <p>Иванов Иван</p>
+                        </td>
+                        <td className={styles.f1Column}>
+                            <p>ООО "Крокодилы"</p>
+                        </td>
+                        <td className={styles.f2Column}>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={stargold} alt="star gold"/>
+                            <img src={star} alt="star gold"/>
+                        </td>
+                        <td className={styles.sColumn}><p>Добавление нового сотрудника</p></td>
+                        <td className={styles.tColumn}>
+                            <p>Пётр Петров</p>
+                        </td>
+                        <td className={styles.foColumn}>
+                            <div className={styles.status}>Зарегистрировано</div>
+                        </td>
+                        <td className={styles.fiColumn}><p>DATAPK</p></td>
+                        <td className={styles.siColumn}><p>Техническое обслуживание</p></td>
+                        <td className={styles.seColumn}><p>22 апреля 2021</p></td>
+                    </tr>
+                </table>
             </div>
         </div>
     )
